8ff2f157e685268bedb9259ba70c1c7d67907aeb
Hemendra Pratap Singh <148527900+Hemendra-Pratap@users.noreply.github.com>
2026-02-10 16:23:31 +0530

    Time: 0 ms (100%), Space: 42.4 MB (24.61%) - LeetHub

---
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static int gcd(int a, int b){
+        if(b==0) return a;
+        return gcd(b,a%b);
+    }
+    public boolean canMeasureWater(int x, int y, int target) {        
+        if(target > x+y) return false;
+        if(x==target || y == target || x+y == target){
+            return true;
+        }
+        return target%gcd(x,y)==0;
+    }
+}
