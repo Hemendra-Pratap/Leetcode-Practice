9c53c27b214a8fc64fdd13b01ca7451db1b3b1bb
Hemendra Pratap Singh <148527900+Hemendra-Pratap@users.noreply.github.com>
2026-02-23 10:26:20 +0530

    Time: 1 ms (56.27%), Space: 42.9 MB (27.59%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int scoreOfParentheses(String s) {
+        Stack<Integer> st  = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '(') st.push(-1);
+            else{
+                if(st.peek()==-1){
+                    st.pop();
+                    st.push(1);
+                }else{
+                    int val = 0;
+                    while(st.peek() != -1){
+                        val += st.peek();
+                        st.pop();
+                    }
+                    st.pop();
+                    st.push(2*val);
+                }
+            } 
+        }
+        int val = 0;
+        while(!st.isEmpty()){
+            val += st.peek();
+            st.pop();
+        }
+        return val;
+
+    }
+}
