b1ea81aec0895dd8a267e157c4b3606f0f4eb6d9
Hemendra Pratap Singh <148527900+Hemendra-Pratap@users.noreply.github.com>
2026-04-11 01:10:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,41 +1,80 @@
+import java.util.*;
+
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        ArrayList<Integer> res = new ArrayList<>();
-        int idx = Arrays.binarySearch(arr, x);
+        LinkedList<Integer> res = new LinkedList<>();
 
-        if(idx < 0){
-            idx = -idx -1; 
+        int idx = Arrays.binarySearch(arr, x);
+        if (idx < 0) {
+            idx = -idx - 1;
         }
 
-        int left = idx-1;
+        int left = idx - 1;
         int right = idx;
-        while(k != 0){
-            if(left < 0 ){
-                res.add(arr[right++]);
-            }else if(right == arr.length){
-                res.add(arr[left--]);
-            }else{
-                if(Math.abs(arr[left]-x) < Math.abs(arr[right]-x)){
-                    res.add(arr[left]);
-                    left--;
-                }else if(Math.abs(arr[left]-x) > Math.abs(arr[right]-x)){
-                    res.add(arr[right]);
-                    right++;
-                }else{
-                    if(arr[left] < arr[right]){
-                        res.add(arr[left]);
-                        left--;
-                    }
-                    else{
-                        res.add(arr[right]);
-                        right++;
+
+        while (k != 0) {
+            if (left < 0) {
+                res.addLast(arr[right++]);
+            } else if (right >= arr.length) {
+                res.addFirst(arr[left--]);
+            } else {
+                if (Math.abs(arr[left] - x) < Math.abs(arr[right] - x)) {
+                    res.addFirst(arr[left--]);
+                } else if (Math.abs(arr[left] - x) > Math.abs(arr[right] - x)) {
+                    res.addLast(arr[right++]);
+                } else {
+                    if (arr[left] < arr[right]) {
+                        res.addFirst(arr[left--]);
+                    } else {
+                        res.addLast(arr[right++]);
                     }
                 }
             }
             k--;
         }
 
-        Collections.sort(res);
         return res;
     }
 }
+// class Solution {
+//     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+//         ArrayList<Integer> res = new ArrayList<>();
+//         int idx = Arrays.binarySearch(arr, x);
+
+//         if(idx < 0){
+//             idx = -idx -1; 
+//         }
+
+//         int left = idx-1;
+//         int right = idx;
+//         while(k != 0){
+//             if(left < 0 ){
+//                 res.add(arr[right++]);
+//             }else if(right == arr.length){
+//                 res.add(arr[left--]);
+//             }else{
+//                 if(Math.abs(arr[left]-x) < Math.abs(arr[right]-x)){
+//                     res.add(arr[left]);
+//                     left--;
+//                 }else if(Math.abs(arr[left]-x) > Math.abs(arr[right]-x)){
+//                     res.add(arr[right]);
+//                     right++;
+//                 }else{
+//                     if(arr[left] < arr[right]){
+//                         res.add(arr[left]);
+//                         left--;
+//                     }
+//                     else{
+//                         res.add(arr[right]);
+//                         right++;
+//                     }
+//                 }
+//             }
+
+//             k--;
+//         }
+
+        
+//         return res;
+//     }
+// }
