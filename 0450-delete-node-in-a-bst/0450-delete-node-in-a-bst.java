2774d95bac3c14c1f0ca4d8b2716c193cbb6cbbf
Hemendra Pratap Singh <148527900+Hemendra-Pratap@users.noreply.github.com>
2026-05-09 00:44:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -1,46 +1,37 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public static TreeNode InOrderSUck(TreeNode root){
-        while(root.left != null){
+
+    public static TreeNode inorderSucc(TreeNode root) {
+        while (root.left != null) {
             root = root.left;
         }
         return root;
     }
-    public TreeNode help(TreeNode root, int key){
-            if(root == null) return null;
-            if(root.val < key)  root.right = help(root.right, key);
-            else if(root.val > key)  root.left = help(root.left, key);
-            else{
-                // no child
-                if(root.left == null && root.right == null){
-                    return null;
-                }
-                if(root.left == null) {
-                    return root.right;
-                }
-                if(root.right == null){
-                    return root.left;
-                }
-                TreeNode IS = InOrderSUck(root.right);
-                root.val  = IS.val;
-                root.right = help(root.right, IS.val);
+    public TreeNode help(TreeNode root, int key) {
+        if (root == null) return null;
+        if (root.val < key) {
+            root.right = help(root.right, key);
+        }else if (root.val > key) {
+            root.left = help(root.left, key);
+        }else {
+            // case 1: no child
+            if (root.left == null && root.right == null) {
+                return null;
             }
-            return root;
+            // case 2: one child
+            if (root.left == null) {
+                return root.right;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+            // case 3: two children
+            TreeNode is = inorderSucc(root.right);
+            root.val = is.val;
+            root.right = help(root.right, is.val);
+        }
+        return root;
     }
+
     public TreeNode deleteNode(TreeNode root, int key) {
         return help(root, key);
     }
