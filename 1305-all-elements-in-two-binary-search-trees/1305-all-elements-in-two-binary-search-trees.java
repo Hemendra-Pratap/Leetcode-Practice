45431478ba565d8944a3b070c7f480a219c7e696
Hemendra Pratap Singh <148527900+Hemendra-Pratap@users.noreply.github.com>
2026-05-12 01:39:47 +0530

    Time: 16 ms (90.35%), Space: 48.6 MB (76.54%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -13,7 +13,7 @@
  *     }
  * }
  */
-class Solution {
+/*class Solution {
     final List<Integer> list = new ArrayList<>();
     public void help(TreeNode root ){
         // if(root1 == null && root2 == null) return ;
@@ -32,5 +32,52 @@ class Solution {
         help(root2);
         Collections.sort(list);
         return list;
+    }
+}
+*/
+class Solution {
+
+    public void inorder(TreeNode root, List<Integer> list) {
+        if (root == null) return;
+
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        inorder(root1, list1);
+        inorder(root2, list2);
+
+        List<Integer> ans = new ArrayList<>();
+
+        int i = 0, j = 0;
+
+        while (i < list1.size() && j < list2.size()) {
+
+            if (list1.get(i) <= list2.get(j)) {
+                ans.add(list1.get(i));
+                i++;
+            } else {
+                ans.add(list2.get(j));
+                j++;
+            }
+        }
+
+        while (i < list1.size()) {
+            ans.add(list1.get(i));
+            i++;
+        }
+
+        while (j < list2.size()) {
+            ans.add(list2.get(j));
+            j++;
+        }
+
+        return ans;
     }
 }
