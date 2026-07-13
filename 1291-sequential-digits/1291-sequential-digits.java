04498d2b6cc94ba6174935f289dd84316d519e4e
Hemendra Pratap Singh <148527900+Hemendra-Pratap@users.noreply.github.com>
2026-07-14 01:39:58 +0530

    Time: 1 ms (28.11%), Space: 42.3 MB (56.22%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+
+        List<Integer> res = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= 9; i++) {
+            q.offer(i);
+        }
+
+        while (!q.isEmpty()) {
+
+            int num = q.poll();
+
+            if (num >= low && num <= high)
+                res.add(num);
+
+            int lastDigit = num % 10;
+
+            if (lastDigit < 9) {
+
+                int next = num * 10 + (lastDigit + 1);
+
+                if (next <= high)
+                    q.offer(next);
+            }
+        }
+
+        return res;
+    }
+}
